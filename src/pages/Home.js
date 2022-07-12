16e77164561117bae46aac4b7efe6fd75778a066
Deepank kartikey <deepankkartikey@gmail.com>
2022-07-12 17:29:31 -0400

    HomePage component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import React, { useState } from 'react';
+import Banner from '../components/Banner';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<Box>
+			<Banner />
+			<SearchExercises />
+			<Exercises />
+		</Box>
+	);
 };
 
 export default Home;
